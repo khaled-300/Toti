514c1d87df5e20171f82a91235749cd8f483fe0d
khaled-300 <kh.aljadaan@gmail.com>
2019-09-22 13:25:41 -0300

    update for navbar
    responsive navbar

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 function toggle() {
-    var responsive = document.getElementById("mynavbar");
+    let responsive = document.getElementById("mynavbar");
     if (responsive.className === "navbar") {
         responsive.className += " responsive";
     } else {
         responsive.className = "navbar";
     }
 }
+
